ecd745d36dee316baa96c9e4ac139ca9de373268
SlicRik8 <151960863+SlicRik8@users.noreply.github.com>
2024-06-22 17:24:51 +0000

    container config

---
+++ 01_basics/01_var.js
@@ -0,0 +1,19 @@
+const accountId = 144553
+let accountEmail = "iamapussy@google.com"
+var accountPassword = "12345"
+
+accountCity = "Jaipur" //this method of declaring a variable without mentioning the datatype is not advisable 
+let accountState; // default value is undefined
+// accountId = 2 not allowed
+accountEmail = "yourapussy@google.com"
+accountPassword = "21212121"
+
+accountCity = "Bangalore"
+/*
+Prefer not to use var 
+because of issue in block scope or functional scope
+*/ 
+
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
+
