f3a69db0fd5ee7c31f4f724140b09a3e7dc4cb27
SlicRik8 <151960863+SlicRik8@users.noreply.github.com>
2024-06-22 19:37:09 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,20 @@
+"use strict"; // treat all the js code as newer version 
+
+//alert(3+3)  we are using nodejs not browser
+
+let name = "Rakshit"
+
+let age = 20
+let isLoggedIn = false
+
+//number => 2 to power 53
+//bigint 
+//string => ""
+//boolean => true/flase
+//null => stand alone value
+// undefined => default value/no value assigned
+//symbol => unique
+
+//object
+
+console.log(typeof undefined);
