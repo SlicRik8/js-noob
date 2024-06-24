4958afda8e369aede49f865df6c23ca52ff98014
SlicRik8 <151960863+SlicRik8@users.noreply.github.com>
2024-06-24 14:35:07 +0000

    conversionOfDataTypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,33 @@
+//let score = "33abc"
+//let score = null
+let score = undefined
+
+
+//const{score} = req.body
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score) // this will convert the score in number
+
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" => 33
+//"33abc" => NaN
+// "true" => 1; false => 0 and vice versa
+
+let isLoggedIn = "rakshit"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+
+//console.log(booleanIsLoggedIn);
+
+// "" => false
+//"rakshit"=>true // non empty string is true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+
+console.log(stringNumber);
+console.log(typeof stringNumber);
