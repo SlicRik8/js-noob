72635f0646faa656f0dc2d7a3dab7e574455dce0
SlicRik8 <151960863+SlicRik8@users.noreply.github.com>
2024-06-25 17:16:49 +0000

    comparison config

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,19 @@
+// console.log("2">1);
+
+// console.log(null>0);  
+// console.log(null<0);
+// console.log(null==0);
+// console.log(null>=0); // this will come true
+
+//  console.log(undefined == 0);
+//  console.log(undefined > 0);
+//  console.log(undefined < 0); 
+
+// should avoid the above comparison in code in most cases 
+
+// strict check ===
+
+console.log("2" === 2); // this will give false as strict check also checks the datatype
+
+
+
