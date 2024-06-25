721d49ee5187e4e2ca19845b12069103caec75b8
SlicRik8 <151960863+SlicRik8@users.noreply.github.com>
2024-06-25 18:51:36 +0000

    comparison config

--- 01_basics/04_comparison.js
+++ 01_basics/04_comparison.js
@@ -11,9 +11,41 @@
 
 // should avoid the above comparison in code in most cases 
 
+
 // strict check ===
 
-console.log("2" === 2); // this will give false as strict check also checks the datatype
+//console.log("2" === 2); // this will give false as strict check also checks the datatype
 
+// symbol
+const id = Symbol("123")
+const anotherId = Symbol("123") // both the values appears to be the same
+                                // but they wont have the same value
 
+console.log(id === anotherId); // this will give false
 
+//const bigNumber = 231234134141312123n //adding n will convert it to bigInt
+
+//console.log(typeof bigNumber);
+
+//Arrays
+
+const heros = ["spiderman","rakshit","chaddhaman"]
+
+//objects
+
+let myObj = {
+    name:"rakshit",   // values under the curly braces are objects
+    age:20,
+}
+
+//function
+
+const myfunction=function(){
+    console.log("Hello World");
+}
+
+const outsideTemp = null
+console.log(typeof outsideTemp); // this will print object as an output
+
+console.log(typeof myfunction); // will give object function or function object as output
+                                // here it will give an output as function
